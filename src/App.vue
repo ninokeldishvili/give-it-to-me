1bf19a208eca685f3e21debac2fac5af120c8be7
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-01-26 01:16:42 +0400

    Add decorator balloons

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-<div class="container">
-  <Header/>
-
-</div>
+  <div class="main">
+    <div class="left-circle">
+      <div class="left-circle-inner"></div>
+    </div>
+    <div class="container">
+      <Header />
+    </div>
+    <div class="right-circle">
+      <div class="right-circle-inner"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header
   }
-}
+};
 </script>
 
 <style>
-  @import 'scss/style.css';
+@import "scss/style.css";
 </style>
