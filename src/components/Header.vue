70169d1258dc671274eb436fb92a782238fb21b2
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-01-26 01:46:01 +0400

    Add yet primitive Input component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,19 @@
 <template>
-<div class="header">
-    <img alt="Vue logo" class="main-logo"  src="../images/logo.png">
-    <div class="search">
-      <input type="text" >
-      <div class="search-btn">Search</div>
-    </div>
+  <div class="header">
+    <img alt="Vue logo" class="main-logo" src="../images/logo.png" />
+    <Input type="text" :hasButton="true" btnText="Search"/>
     <div class="user-settings">
       <div class="user">USER</div>
       <div class="lang-switch">EN</div>
     </div>
-    
-</div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Header',
-}
-</script>
+import Input from "../components/Input";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "Header",
+  components: { Input }
+};
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="nk-input">
+      <input :type="type" />
+      <div class="nk-btn" v-if="hasButton">{{btnText}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    hasButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    btnText:{
+        type: String,
+        required: false
+    }
+  }
+};
+</script>
