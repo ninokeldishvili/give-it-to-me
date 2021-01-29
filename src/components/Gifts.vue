6bdaaee7dda8e5bb8cb0bd59af351b6ea9f9dedc
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-01-30 00:51:29 +0400

    Bind link to view button

--- src/components/Gifts.vue
+++ src/components/Gifts.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="gift-list">
     <div class="gifts" v-for="gift in allGifts" :key="gift.id">
-      <a :href="gift.url" target="blank">
-        <div class="gift-card">
-          <div class="img-container">
-            <img :src="gift.img_url" alt="gift-for-you" />
-            <div class="overlay"></div>
-            <div class="option-btns">
-              <Button text="View" />
-              <Button text="Remove" />
-            </div>
+      <div class="gift-card">
+        <div class="img-container">
+          <img :src="gift.img_url" alt="gift-for-you" />
+          <div class="overlay"></div>
+          <div class="option-btns">
+            <a :href="gift.url" target="blank">
+              <Button text="View"/>
+            </a>
+            <Button text="Remove" />
           </div>
-          <span>{{ gift.title }}</span>
         </div>
-      </a>
+        <span>{{ gift.title }}</span>
+      </div>
     </div>
   </div>
 </template>
