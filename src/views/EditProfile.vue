b480078d6dec41fbbe57d3c85c7963f499edde4e
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-02-08 20:29:09 +0400

    Add Edit-Profile view with draft design

---
+++ src/views/EditProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="edit-profile container">
+    <Input label="First Name" />
+    <Input label="Last Name" />
+    <Input label="Phone Number" />
+    <Input label="Email" />
+    <Input label="Address" />
+    <Button text="Submit" />
+  </div>
+</template>
+
+<script>
+import Input from "../components/Input.vue";
+import Button from "../components/Button.vue";
+
+export default {
+  name: "EditProfile",
+  components: {
+    Input,
+    Button
+  }
+};
+</script>
