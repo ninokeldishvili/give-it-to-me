56278fff3583eb006ab3211c4ec17ae3629ff8f2
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-02-09 01:36:38 +0400

    Editprofile view styles

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="edit-profile container">
-    <Input type="text" label="First Name" />
-    <Input type="text" label="Last Name" />
-    <Input type="text" label="Phone Number" />
-    <Input type="text" label="Email" />
-    <Input type="text" label="Address" />
-    <Button text="Submit" />
+    <div class="user-avatar">
+      <font-awesome-icon icon="user" class="icon" />
+    </div>
+    <div class="user-info-form-container">
+      <Input type="text" label="First Name" />
+      <Input type="text" label="Last Name" />
+      <Input type="text" label="Phone Number" />
+      <Input type="text" label="Email" />
+      <Input type="text" label="Address" />
+      <Button text="Submit" />
+    </div>
   </div>
 </template>
 
