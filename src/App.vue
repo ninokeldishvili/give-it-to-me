9fe532e1bd759ef25ff0e4c66fdecb298698cc0f
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-02-10 23:23:03 +0400

    Put header in Header component

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,27 @@
 <template>
-  <div class="main">
-    <div class="left-circle">
-      <div class="left-circle-inner"></div>
-    </div>
-    <div class="container">
-      <div class="header">
-        <router-link to="/"
-          ><img alt="logo" class="main-logo" src="./images/logo.png"
-        /></router-link>
-        <div class="search">
-          <Input type="text" v-model="user" />
-          <div @click="onSearch()" style="margin:auto 0">
-            <Button text="Search" />
-          </div>
-          <div v-if="user" class="search-items">
-            <ul>
-              <li v-for="user in searchUsers" :key="user.id">
-                {{ user.firstName }} {{user.lastName}}
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="user-settings">
-          <div class="user">
-            <font-awesome-icon icon="user" class="icon" />
-            <div class="profile-dropdown">
-              <ul>
-                <li><router-link to="/">My Wishes</router-link></li>
-                <li>
-                  <router-link to="/edit-profile">Edit Profile</router-link>
-                </li>
-                <li><router-link to="/">Sign Out</router-link></li>
-              </ul>
-            </div>
-          </div>
-          <div class="lang-switch">EN</div>
-        </div>
+  <div>
+    <div class="main">
+      <div class="left-circle">
+        <div class="left-circle-inner"></div>
+      </div>
+      <div class="container">
+        <Header />
+      </div>
+      <div class="right-circle">
+        <div class="right-circle-inner"></div>
       </div>
     </div>
-    <div class="right-circle">
-      <div class="right-circle-inner"></div>
-    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <script>
-import Input from "./components/Input.vue";
-import Button from "./components/Button.vue";
-import { mapState, mapActions } from "vuex";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    Input,
-    Button
-  },
-  data() {
-    return {
-      user: ""
-      // searchUsers: []
-    };
-  },
-  computed: {
-    ...mapState(["currentUser", "users"]),
-    searchUsers() {
-      return this.users.filter(
-        u =>
-          u.firstName.toLowerCase().includes(this.user.toLowerCase()) ||
-          u.lastName.toLowerCase().includes(this.user.toLowerCase())
-      );
-    }
-  },
-  mounted() {
-    this.fetchUsers();
-  },
-  methods: {
-    ...mapActions(["fetchUsers"])
+    Header
   }
 };
 </script> 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="header">
-    <img alt="Vue logo" class="main-logo" src="../images/logo.png" />
-    <Input type="text" :hasButton="true" btnText="Search" />
+    <router-link to="/">
+      <img alt="logo" class="main-logo" src="../images/logo.png"/>
+    </router-link>
+    <div class="search">
+      <Input type="text" v-model="user" />
+      <div @click="onSearch()" style="margin:auto 0">
+        <Button text="Search" />
+      </div>
+      <div v-if="user" class="search-items">
+        <ul>
+          <li v-for="user in searchUsers" :key="user.id">
+            {{ user.firstName }} {{ user.lastName }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
     <div class="user-settings">
       <div class="user">
-        <font-awesome-icon icon="user" class="icon" /></div>
+        <font-awesome-icon icon="user" class="icon" />
+        <div class="profile-dropdown">
+          <ul>
+            <li><router-link to="/">My Wishes</router-link></li>
+            <li>
+              <router-link to="/edit-profile">Edit Profile</router-link>
+            </li>
+            <li><router-link to="/">Sign Out</router-link></li>
+          </ul>
+        </div>
+      </div>
       <div class="lang-switch">EN</div>
     </div>
   </div>
@@ -12,9 +37,32 @@
 
 <script>
 import Input from "../components/Input";
+import Button from "../components/Button";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "Header",
-  components: { Input }
+  components: { Input, Button },
+  data() {
+    return {
+      user: ""
+    };
+  },
+  computed: {
+    ...mapState(["currentUser", "users"]),
+    searchUsers() {
+      return this.users.filter(
+        u =>
+          u.firstName.toLowerCase().includes(this.user.toLowerCase()) ||
+          u.lastName.toLowerCase().includes(this.user.toLowerCase())
+      );
+    }
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    ...mapActions(["fetchUsers"])
+  }
 };
 </script>
