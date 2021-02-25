bf8d7e2c72c7cd08bcb811bcf801771fa490bca4
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-02-26 01:16:27 +0400

    Search Responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,60 @@
 <template>
-  <div class="header">
-    <router-link to="/">
-      <img
-        @click="goToMyWishes()"
-        alt="logo"
-        class="main-logo"
-        src="../images/logo.png"
-      />
-    </router-link>
-    <div class="search">
-      <Input type="text" v-model="inputUser" />
-      <div @click="onSearch()" style="margin:auto 0">
-        <Button text="Search" />
+  <div>
+    <div class="header">
+      <router-link to="/">
+        <img
+          @click="goToMyWishes()"
+          alt="logo"
+          class="main-logo"
+          src="../images/logo.png"
+        />
+      </router-link>
+      <div class="header-right">
+        <div class="user-settings">
+          <div class="user">
+            <font-awesome-icon icon="user" class="icon" />
+            <div class="profile-dropdown">
+              <ul>
+                <router-link to="/">
+                  <li @click="goToMyWishes()">My Wishes</li>
+                </router-link>
+                <router-link to="/profile">
+                  <li @click="getUser(loggedUser.id)">My Profile</li>
+                </router-link>
+                <router-link to="/login">
+                  <li @click="signOut()">Sign Out</li>
+                </router-link>
+              </ul>
+            </div>
+          </div>
+          <div class="lang-switch">EN</div>
+        </div>
+        <div class="search">
+          <Input type="text" v-model="inputUser" />
+          <div @click="onSearch()" style="margin:auto 0">
+            <Button text="Search" />
+          </div>
+          <div v-if="inputUser" class="search-items">
+            <ul>
+              <li
+                v-for="user in searchUsers"
+                :key="user.id"
+                @click="getSelectedUser(user.id)"
+              >
+                {{ user.firstName }} {{ user.lastName }}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div v-if="inputUser" class="search-items">
-        <ul>
+    </div>
+      <div class="search-responsive">
+        <Input type="text" v-model="inputUser" />
+        <div @click="onSearch()" style="margin:auto 0">
+          <Button text="Search" />
+        </div>
+        <div v-if="inputUser" class="search-items">
+          <ul>
             <li
               v-for="user in searchUsers"
               :key="user.id"
@@ -22,29 +62,9 @@
             >
               {{ user.firstName }} {{ user.lastName }}
             </li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="user-settings">
-      <div class="user">
-        <font-awesome-icon icon="user" class="icon" />
-        <div class="profile-dropdown">
-          <ul>
-            <router-link to="/">
-              <li @click="goToMyWishes()">My Wishes</li>
-            </router-link>
-            <router-link to="/profile">
-              <li @click="getUser(loggedUser.id)">My Profile</li>
-            </router-link>
-            <router-link to="/login">
-              <li @click="signOut()">Sign Out</li>
-            </router-link>
           </ul>
         </div>
       </div>
-      <div class="lang-switch">EN</div>
-    </div>
   </div>
 </template>
 
@@ -76,14 +96,14 @@ export default {
     this.fetchUsers();
   },
   methods: {
-    ...mapActions(["fetchUsers", "getUser","signOut"]),
+    ...mapActions(["fetchUsers", "getUser", "signOut"]),
     getSelectedUser(userId) {
       this.selectedUser = this.users.find(u => u.id === userId);
       this.inputUser =
         this.selectedUser.firstName + " " + this.selectedUser.lastName;
     },
     onSearch() {
-      if(!this.inputUser){
+      if (!this.inputUser) {
         return;
       }
       this.getUser(this.selectedUser.id);
